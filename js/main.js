3d2b5c0795c01507dea60d1d9ae270959d632284
nasu-world <nasuworld1110@gmail.com>
2024-10-05 09:57:09 +0900

    fix path

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-fetch("include/header.html")
+fetch("/include/header.html")
   .then((response) => response.text())
   .then((data) => document.querySelector("#header").innerHTML = data);
-fetch("include/footer.html")
+fetch("/include/footer.html")
     .then((response) => response.text())
     .then((data) => document.querySelector("#footer").innerHTML = data);
